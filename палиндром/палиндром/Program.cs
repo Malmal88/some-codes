979f854b2e7d3d63ca55eae4788167b811211a78
Malmal88 <125454029+Malmal88@users.noreply.github.com>
2023-03-07 12:32:17 +0700

    Add files via upload

---
+++ палиндром/палиндром/Program.cs
@@ -0,0 +1,17 @@
+﻿
+            bool ispalindrome = true;
+            Console.WriteLine("Введите текст");
+
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length/2; i++)
+            {               
+                 if (str[i] != str[str.Length-1-i]) 
+                    {
+                        ispalindrome = false;
+                        break; 
+                    }                                                   
+            }
+            if (ispalindrome) Console.WriteLine("палиндром");
+            else Console.WriteLine("не палиндром");
+
---
+++ ученики/ученики/Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        string[] name = { "Ваня", "Петя", "Коля", "Саша" };
+        int[] height = new int[name.Length];
+        int[] weight = new int[name.Length];
+        float[] IMT = new float[name.Length];
+
+        void Init()
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                height[i] = random.Next(150, 200);
+                weight[i] = random.Next(50, 100);
+                IMT[i] = height[i] / (float)weight[i];
+            }
+        }
+
+        static int  Calcmax(int[] data)
+        {
+            int max = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[max] < data[i]) max = i;
+            }
+            return max;
+        }
+
+        static int CalcmaxIMT(float[] data)
+        {
+            int max = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[max] < data[i]) max = i;
+            }
+            return max;
+        }
+
+        void Print()
+        {
+            for (int i = 0;i < height.Length;i++)
+            {
+                Console.WriteLine($"   {name[i]}");
+                Console.WriteLine($" Рост {height[i]}");
+                Console.WriteLine($" Вес  {weight[i]}");
+                Console.WriteLine($" ИМТ  {IMT[i]}");
+            }
+         Console.WriteLine($" Самый высокий :{name[Calcmax(height)]};  Самый жирный: {name[Calcmax(weight)]}; Максимальный ИМТ:{name[CalcmaxIMT(IMT)]} ");
+                       
+        }
+
+        Init();
+        Print();
+    }
+
+}
