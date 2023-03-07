31965457c512bbef094d2483739d9daa99776838
Malmal88 <125454029+Malmal88@users.noreply.github.com>
2023-03-07 12:30:40 +0700

    Add files via upload

---
+++ buble sort/обмен местами/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random random = new Random();
+int N=int.Parse(Console.ReadLine());
+int [] nums = new int [N] ;
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = random.Next(1, N);
+    Console.Write($"  {nums[i]}");
+}
+
+while (true)
+{
+   bool f = false;
+   for (int i = 1; i < nums.Length; i++)
+   {
+        int temp = nums[i-1];
+        if (nums[i] < nums[i - 1])
+        {
+            f = true;
+            nums[i-1]=nums[i];
+            nums[i] = temp;
+
+        }
+   }
+    if (!f) break;
+}
+Console.WriteLine();
+for (int i = 0; i < nums.Length; i++) Console.Write($"  {nums[i]}");
